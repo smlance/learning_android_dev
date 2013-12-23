abc7ae55362fe636ae702326454e6aefb7ba5412
smlance <stefan@lances.net>
2013-12-22 21:20:04 -0500

    Fixed AndroidManifest.xml DisplayMessageActivity declaration

--- gen/R.java.d
+++ gen/R.java.d
@@ -10,4 +10,12 @@
 /home/stefan/code/android/res/drawable-ldpi/ic_launcher.png \
 /home/stefan/code/android/res/drawable-hdpi/ic_action_search.png \
 /home/stefan/code/android/res/drawable-hdpi/ic_launcher.png \
+/home/stefan/code/android/bin/res/drawable-xxhdpi/ic_action_search.png \
+/home/stefan/code/android/bin/res/drawable-mdpi/ic_action_search.png \
+/home/stefan/code/android/bin/res/drawable-mdpi/ic_launcher.png \
+/home/stefan/code/android/bin/res/drawable-xhdpi/ic_action_search.png \
+/home/stefan/code/android/bin/res/drawable-xhdpi/ic_launcher.png \
+/home/stefan/code/android/bin/res/drawable-ldpi/ic_launcher.png \
+/home/stefan/code/android/bin/res/drawable-hdpi/ic_action_search.png \
+/home/stefan/code/android/bin/res/drawable-hdpi/ic_launcher.png \
 /home/stefan/code/android/bin/AndroidManifest.xml \
